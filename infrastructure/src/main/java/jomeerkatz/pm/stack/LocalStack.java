b58b5acb69efd62c34efb22ba5ce4075d8ab319c
jomeerkatz <jokirillmeerkatz@outlook.de>
2026-01-31 13:51:31 +0100

    feat: add database service

--- infrastructure/src/main/java/jomeerkatz/pm/stack/LocalStack.java
+++ infrastructure/src/main/java/jomeerkatz/pm/stack/LocalStack.java
@@ -1,18 +1,26 @@
 package jomeerkatz.pm.stack;
 
 import software.amazon.awscdk.*;
+import software.amazon.awscdk.services.ec2.InstanceClass;
+import software.amazon.awscdk.services.ec2.InstanceSize;
+import software.amazon.awscdk.services.ec2.InstanceType;
 import software.amazon.awscdk.services.ec2.Vpc;
+import software.amazon.awscdk.services.rds.*;
 
 // cdk: cloud development kid -> for creating cloud infra but with java code and NOT
 // with CloudFormation code/syntax
 // so what we can do is describing how the aws infra should look like
-public class LocalStack extends Stack {
+public class LocalStack extends Stack { // stack class, where we define which ressources are in the stack
     private final Vpc vpc;
 
     // constructor for creating a stack
     public LocalStack(final App scope, final String id, final StackProps props) {
         super(scope, id, props);
         this.vpc = createVpc();
+
+        DatabaseInstance authServiceDB = createDatabase("AuthServiceDB", "auth-service-db");
+
+        DatabaseInstance patientServiceDB = createDatabase("PatientServiceDB", "patient-service-db");
     }
 
     private Vpc createVpc() {
@@ -20,9 +28,24 @@ public class LocalStack extends Stack {
                 .maxAzs(2).build(); // max available zones
     }
 
+    private DatabaseInstance createDatabase(String id, String dbName) {
+        // construct, which will create a database
+        return DatabaseInstance.Builder
+                .create(this, id)
+                .engine(DatabaseInstanceEngine.postgres(PostgresInstanceEngineProps.builder()
+                        .version(PostgresEngineVersion.VER_17_2).build()))
+                .vpc(this.vpc)
+                .instanceType(InstanceType.of(InstanceClass.BURSTABLE2, InstanceSize.MICRO)) // doesnt matter that much bec of local dev
+                .allocatedStorage(20)
+                .credentials(Credentials.fromGeneratedSecret("admin_user"))
+                .databaseName(dbName)
+                .removalPolicy(RemovalPolicy.DESTROY)
+                .build();
+    }
+
     public static void main(final String[] args) {
         App app = new App(AppProps.builder() // create new CDK project
-                .outdir(". /cdk.out") // define, where we want to store the CloudFormation file
+                .outdir("./cdk.out") // define, where we want to store the CloudFormation file
                 .build()); // no resources created yet, just the "wrapper"
 
         StackProps props = StackProps.builder() // create config for stack
